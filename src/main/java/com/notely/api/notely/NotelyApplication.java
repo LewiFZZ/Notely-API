2360c2a130f72f60ee06b9816d3d238f2befdf38
levijcdf <levijcfigueiredowebdev@gmail.com>
2025-10-15 16:27:27 +0100

    Initial Commit

---
+++ src/main/java/com/notely/api/notely/NotelyApplication.java
@@ -0,0 +1,13 @@
+package com.notely.api.notely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotelyApplication.class, args);
+	}
+
+}
